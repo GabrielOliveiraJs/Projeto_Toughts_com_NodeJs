c84d0546e33466ae344eadccb09bb9f07c0f3626
Gabriel de Oliveira Pinto <99537756+GabrielOliveiraJs@users.noreply.github.com>
2022-04-27 16:18:30 -0300

    Add files via upload

---
+++ Toughts/Pensamento/routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const AuthController = require('../controllers/AuthController')
+
+
+router.get('/login', AuthController.login)
+router.post('/login', AuthController.loginPost) //rota de login
+router.get('/register', AuthController.register)
+router.post('/register', AuthController.registerPost) //rota de mensagem de erro 
+router.get('/logout', AuthController.logout) //rota do logout
+
+module.exports = router
